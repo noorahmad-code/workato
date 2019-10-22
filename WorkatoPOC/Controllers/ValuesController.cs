5e8c98486205a0a69db42eeec017f77f09110e17
ahno <ahno@ciklum.com>
2019-10-22 13:08:40 +0500

    Add project files.

---
+++ WorkatoPOC/Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WorkatoPOC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        { 
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public TestPOST Post([FromForm] string value)
+        {
+            TestPOST testPOST = new TestPOST();
+            testPOST.ReturnValue = "We received your request from WorkAto, our support team is working on it. Your email address is " + value;
+            return testPOST;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+    public class TestPOST
+    {
+        public string ReturnValue { get; set; }
+    }
+}
